aa653bccd2fca42b63b84b20c8638ac0e0650edb
vikash2806 <vikashrajendran2806@gmail.com>
2021-01-11 11:52:48 +0530

    getting error while displaying movies

---
+++ 2/script.js
@@ -0,0 +1,78 @@
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3d21ce49abb89fb75081fbf726f18b8e`;
+const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=3d21ce49abb89fb75081fbf726f18b8e&query= `;
+
+//  NOTE api_key=3d21ce49abb89fb75081fbf726f18b8e
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+// get initial movie 
+getMovies(API_URL);
+
+async function getMovies(url)
+{
+    const res = await fetch(url)
+    const data = await res.json()
+
+    function showMovies(movies) 
+    {
+        main.innerHTML = "";
+
+        movie.foreach((movies) =>
+        {
+            const {title, poster_path, vote_average,overview }= movie;
+
+            const movieEl = document.createElement('div')
+            movieEl.classList.add("movie")
+            movieEl.innerHTML =`
+            <img src="${IMG_PATH + poster_path}"alt="${title}">
+            <div class="movie-info">
+                         <h3>${title}</h3>
+                        <span class="${getClassByRate}">${vote_average}/span>
+            </div>
+            <div class="overview">
+                     <h3>overview</h3>
+                         ${overview}
+                </div>
+            </div>   
+         `
+        main.appendChild(movieEl)
+    });
+
+}
+
+function getClassByRate(vote)
+{
+    if(vote >= 8)
+    {
+        return 'greem';
+    }
+    else if (vote >= 5)
+    {
+        return "orange";
+    }
+    else
+    {
+        return "red";
+    }
+}
+
+form.addEventListener('submit',(e)=> 
+{
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== "")
+    {
+        getMovies(SEARCH_API+searchTerm);
+        search.value = "";
+    }
+    else
+    {
+        window.location.reload();
+    }
+});
+
