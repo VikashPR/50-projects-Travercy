2966649e0a14f4b238843bb040493e0a8476a4c1
vikash2806 <vikashrajendran2806@gmail.com>
2021-01-12 08:37:18 +0530

    searching movie , poopular movie fun()  included

--- movie-site/script.js
+++ movie-site/script.js
@@ -15,41 +15,41 @@ async function getMovies(url)
 {
     const res = await fetch(url)
     const data = await res.json()
+    showMovies(data.results);
 
-    function showMovies(movies) 
-    {
-        main.innerHTML = "";
+}
+function showMovies(movies)
+{
+    main.innerHTML = "";
+    movies.forEach(movie => {
+        const { title , poster_path , vote_average , overview , release_date} = movie
 
-        movie.foreach((movies) =>
-        {
-            const {title, poster_path, vote_average,overview }= movie;
+        const movieEl =document.createElement("div");
 
-            const movieEl = document.createElement('div')
-            movieEl.classList.add("movie")
-            movieEl.innerHTML =`
-            <img src="${IMG_PATH + poster_path}"alt="${title}">
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-                         <h3>${title}</h3>
-                        <span class="${getClassByRate}">${vote_average}/span>
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class="overview">
-                     <h3>overview</h3>
-                         ${overview}
-                </div>
-            </div>   
-         `
-        main.appendChild(movieEl)
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl);
     });
-
 }
 
 function getClassByRate(vote)
 {
-    if(vote >= 8)
+    if(vote >= 8 )
     {
-        return 'greem';
+        return "green";
     }
-    else if (vote >= 5)
+    else if(vote >5 && vote<= 8)
     {
         return "orange";
     }
@@ -58,7 +58,6 @@ function getClassByRate(vote)
         return "red";
     }
 }
-
 form.addEventListener('submit',(e)=> 
 {
     e.preventDefault()
@@ -75,4 +74,6 @@ form.addEventListener('submit',(e)=>
         window.location.reload();
     }
 });
+
+
 
